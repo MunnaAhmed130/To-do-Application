19d9956cd82d253e8081ec302350e191dafc05da
MunnaAhmed130 <munnaahmed2025@gmail.com>
2022-02-25 20:11:55 +0600

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useEffect, useReducer, useRef, useState } from 'react';
 import './App.css';
 
+interface Todo {
+  id: number,
+  text: string,
+}
+
+type ActionType =
+  | { type: "ADD"; text: string }
+  | { type: "REMOVE"; id: number };
+
+
 function App() {
+
+  const [myState, setMyState] = useState<Todo[]>();
+  // console.log(myState)
+
+  useEffect(() => {
+
+    // localStorage.setItem()
+  }, [])
+
+  function reducer(state: Todo[], action: ActionType) {
+    switch (action.type) {
+      case "ADD":
+        return [
+          ...state,
+          {
+            id: state.length,
+            text: action.text
+          },
+        ];
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id);
+    }
+  }
+
+  // console.log(todos)
+  const [todos, dispatch] = useReducer(reducer, []);
+
+  const todoRef = useRef<HTMLInputElement>(null);
+
+  const onAdd = useCallback(() => {
+    if (todoRef.current) {
+      dispatch({
+        type: "ADD",
+        text: todoRef.current.value
+      })
+      todoRef.current.value = "";
+    }
+  }, [])
+
+  const onRemove = (id: number) => {
+    dispatch({
+      type: "REMOVE",
+      id: id
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Todos /> */}
+      <input type="text" ref={todoRef} /> <br />
+      <button onClick={onAdd}>Add</button>
+      {
+        todos.map((todo) => (
+          <div key={todo.id}>{todo.text} <button onClick={() => onRemove(todo.id)}>Remove</button></div>
+
+        ))
+      }
     </div>
   );
 }
